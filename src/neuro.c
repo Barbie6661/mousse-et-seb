0fb5ba86a106c055a2c24cc447bd5ac38810b6fd
moussmousse <anthonymousset@hotmail.fr>
2017-10-20 00:44:07 +0200

    .

--- src/neuro.c
+++ src/neuro.c
@@ -17,21 +17,37 @@ struct neuro *init(){
 }
 
 
-void set_values_learn(struct neuro *neuro,float arg1, float arg2)
+void set_values_learn(struct neuro *neuro,float arg1, float arg2,float arg3)
 {
   struct neuro *e1 = init();
   e1 -> p = arg1;
   struct neuro *e2 = init ();
-  e2 -> p = 1.;
+  e2 -> p = arg2;
   struct neuro *e3 = init ();
-  e3 -> p = arg2;
+  e3 -> p = arg3;
 
   neuro -> e1 = e1;
   neuro -> e2 = e2;
   neuro -> e3 = e3;
+
+  //L1 L2 L3
+
 }
 
-void calc_neuro (struct neuro *neuro)
+void calc_neuro (struct neuro *n)
 {
-  neuro -> p = (float)(1/(1+exp((double)(e1*l1+e2*l2+e3*l3))));
+  neuro->p=(float)(1/(1+exp((double)(n->e1+n->l1+n->e2*n->l2+n->e3*n->l3))));
 }
+
+void call_train(float arg1, float arg2)
+{
+  struct neuro *n1 = init();
+  set_values_learn(n1,arg1,1.,arg2);
+  calc_neuro(n1);
+  struct neuro *n2 = init();
+  set_values_neuro(n2,arg1,n1->p,arg2);
+  calc_neuro(n2);
+
+}
+
+
